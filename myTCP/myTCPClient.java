380c52ec1b06c98410a1ac0e149d9088f8f88688
Landixbtw <68870626+Landixbtw@users.noreply.github.com>
2025-05-05 21:01:43 +0200

    .

--- myTCP/myTCPClient.java
+++ myTCP/myTCPClient.java
@@ -3,28 +3,48 @@ import java.net.*;
 
 public class myTCPClient{
   public static void main(String args[]){
-    BufferedReader inFromUser = 
-      new BufferedReader(
-        new InputStreamReader(System.in));	
+    BufferedReader inFromUser =
+            new BufferedReader(
+                    new InputStreamReader(System.in));
 
-    Integer port = 1444;
+    Integer port = 1446;
 
     try{
       Socket clientSocket = new Socket("localhost",port);
-	
-      BufferedReader inFromServer = 
-        new BufferedReader(
-          new InputStreamReader(clientSocket.getInputStream()));
 
-      PrintWriter printToServer = 
-        new PrintWriter(clientSocket.getOutputStream(), true);
+      BufferedReader inFromServer =
+              new BufferedReader(
+                      new InputStreamReader(clientSocket.getInputStream()));
 
-      System.out.print("Type in a line:\n");
-      String userinput = inFromUser.readLine();
-      printToServer.println(userinput);
+      PrintWriter printToServer =
+              new PrintWriter(clientSocket.getOutputStream(), true);
+
+
+
+      //int i = 0;
+      String[] input = new String[128];
+      String userInput = "";
+
+      //do {
+      //  System.out.print("Type in a line: ");
+      //userInput = inFromUser.readLine();
+      //input[i++] = userInput;
+      //} while(inFromUser.readLine() != null);
+      for(int i = 0; i < 3; i++) {
+        System.out.print("Type in a line: ");
+        userInput = inFromUser.readLine();
+        input[i] = userInput;
+        System.out.println(input[i]);
+      }
+
+      for(int i = 0; i < 3; i++) {
+        System.out.println("Sending to server ");
+        printToServer.println(input[i]);
+      }
 
       String response = inFromServer.readLine();
       System.out.println("Server's response:\n" + response);
+
       clientSocket.close();
     } catch (Exception e){e.printStackTrace();}
 
--- myTCP/myTCPServer.java
+++ myTCP/myTCPServer.java
@@ -3,22 +3,25 @@ import java.net.*;
 
 public class myTCPServer{
   public static void main(String args[]){
-    Integer port=1444;		
+    Integer port=1446;
     try{
       if (args.length >= 2 && args[0].equals("-port"))
         port=Integer.parseInt(args[1]);
 
       System.out.println("Listen to Port: " + port);
       ServerSocket welcomeSocket = new ServerSocket(port);
-			
+
       while(true) {
         System.out.println("Waiting for Client to connect...");
         Socket connectionSocket = welcomeSocket.accept();
 
         BufferedReader inFromClient = new BufferedReader(
-          new InputStreamReader(connectionSocket.getInputStream()));
+                new InputStreamReader(connectionSocket.getInputStream()));
 
         String request= inFromClient.readLine();
+
+        // TODO:
+        // inFromClient.ready() -> true mit readln()
 
         String output = request.toUpperCase();
         OutputStream os = connectionSocket.getOutputStream();
@@ -27,7 +30,7 @@ public class myTCPServer{
         connectionSocket.close();
       }
     } catch (IOException e){
-        e.printStackTrace();
-      }
+      e.printStackTrace();
+    }
   }
 }
