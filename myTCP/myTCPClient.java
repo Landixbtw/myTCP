47fc6dd16e0753b918e2349799d0a08ee5b6b81d
Landixbtw <68870626+Landixbtw@users.noreply.github.com>
2025-05-06 21:34:23 +0200

    .

--- myTCP/myTCPClient.java
+++ myTCP/myTCPClient.java
@@ -2,49 +2,49 @@ import java.io.*;
 import java.net.*;
 
 public class myTCPClient{
-  public static void main(String args[]){
-    BufferedReader inFromUser =
+    public static void main(String args[]){
+        BufferedReader inFromUser =
             new BufferedReader(
                     new InputStreamReader(System.in));
 
-    Integer port = 1446;
+        Integer port = 1446;
 
-    try{
-      Socket clientSocket = new Socket("localhost",port);
+        try{
+            Socket clientSocket = new Socket("localhost",port);
 
-      BufferedReader inFromServer =
+            BufferedReader inFromServer =
               new BufferedReader(
                       new InputStreamReader(clientSocket.getInputStream()));
 
-      PrintWriter printToServer =
+            PrintWriter printToServer =
               new PrintWriter(clientSocket.getOutputStream(), true);
 
 
-        // TODO: adjustable size array?
-        String[] input = new String[128];
-        String userInput = "";
+            // TODO: adjustable size array?
+            String[] input = new String[128];
+            String userInput = "";
 
-        int i = 0;
-        while(true) {
-            System.out.print("Type in a line: ");
-            userInput = inFromUser.readLine();
-            if(userInput == null) break;
-            input[i] = userInput;
-            i++;
-        }
+            int i = 0;
+            while(true) {
+                System.out.print("Type in a line: ");
+                userInput = inFromUser.readLine();
+                if(userInput == null) break;
+                input[i] = userInput;
+                i++;
+            }
 
-        for(int j = 0; j < i; j++) {
-            System.out.println("Sending to server ");
-            printToServer.println(input[j]);
-        }
+            for(int j = 0; j < i; j++) {
+                System.out.println("Sending to server ");
+                printToServer.println(input[j]);
+            }
 
             clientSocket.shutdownOutput();
             System.out.println("waiting for response");
-        String response = inFromServer.readLine();
-        System.out.println("Server's response:\n" + response);
+            String response = inFromServer.readLine();
+            System.out.println("Server's response:\n" + response);
 
-        clientSocket.close();
-    } catch (Exception e){e.printStackTrace();}
+            clientSocket.close();
+        } catch (Exception e){e.printStackTrace();}
 
-  }
+    }
 }
